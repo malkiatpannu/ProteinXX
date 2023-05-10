760f4516d2b359142476e422a0a873534ec2c778
malkiat pannu <malkiatpannu@gmail.com>
2023-05-10 20:56:26 +0530

    added fixed header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,123 +1,136 @@
-$(document).ready(function(){
-	$('header .mobile-menu').click(function(){
-		$(this).parents('header').toggleClass('mobile-active');
-	});
+$(document).ready(function () {
+    $('header .mobile-menu').click(function () {
+        $(this).parents('header').toggleClass('mobile-active');
+    });
 
-	// Select all links with hashes
-	$('a.scroll')
-	  // Remove links that don't actually link to anything
-	  .not('[href="#"]')
-	  .not('[href="#0"]')
-	  .click(function(event) {
-	    // On-page links
-	    if (
-	      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-	      && 
-	      location.hostname == this.hostname
-	    ) {
-	      // Figure out element to scroll to
-	      var target = $(this.hash);
-	      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-	      // Does a scroll target exist?
-	      if (target.length) {
-	        // Only prevent default if animation is actually gonna happen
-	        event.preventDefault();
-	        $('html, body').animate({
-	          scrollTop: target.offset().top
-	        }, 1000, function() {
-	          // Callback after animation
-	          // Must change focus!
-	          var $target = $(target);
-	          $target.focus();
-	          if ($target.is(":focus")) { // Checking if the target was focused
-	            return false;
-	          } else {
-	            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-	            $target.focus(); // Set focus again
-	          };
-	        });
-	      }
-	    }
-	  });
-
-
-		jQuery('#contact-form').submit(function(e){
-				var validated = true;
-				var form = $(this);
-
-				form.find('input[required],textarea[required]').each(function(){
-					if( $(this).val() == '' ) {
-						$(this).addClass('input-error');
-						validated = false;
-					}
-					else {
-						$(this).removeClass('input-error');
-					}
-				});
-
-				console.log('Validated 1' + validated);
+    // Select all links with hashes
+    $('a.scroll')
+            // Remove links that don't actually link to anything
+            .not('[href="#"]')
+            .not('[href="#0"]')
+            .click(function (event) {
+                // On-page links
+                if (
+                        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+                        &&
+                        location.hostname == this.hostname
+                        ) {
+                    // Figure out element to scroll to
+                    var target = $(this.hash);
+                    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                    // Does a scroll target exist?
+                    if (target.length) {
+                        // Only prevent default if animation is actually gonna happen
+                        event.preventDefault();
+                        $('html, body').animate({
+                            scrollTop: target.offset().top
+                        }, 1000, function () {
+                            // Callback after animation
+                            // Must change focus!
+                            var $target = $(target);
+                            $target.focus();
+                            if ($target.is(":focus")) { // Checking if the target was focused
+                                return false;
+                            } else {
+                                $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                                $target.focus(); // Set focus again
+                            }
+                            ;
+                        });
+                    }
+                }
+            });
 
 
-				// $.ajax({
-				// 	url: '/mailer/recaptcha.php',
-				// 	type: 'POST',
-				// 	data: form.serialize(),
-				// 	error: function() {
-				// 		alert('An error occured while processing the request. Please try again later.');
-				// 	},
-				// 	complete : function(data) {
+    jQuery('#contact-form').submit(function (e) {
+        var validated = true;
+        var form = $(this);
 
-				// 		if(data.responseText == '') {
-				// 			validated = true;
-				// 			jQuery('#recaptcha-msg').html('');
-				// 		} else {
-				// 			validated = false;
-				// 			jQuery('#recaptcha-msg').html(data.responseText);
+        form.find('input[required],textarea[required]').each(function () {
+            if ($(this).val() == '') {
+                $(this).addClass('input-error');
+                validated = false;
+            } else {
+                $(this).removeClass('input-error');
+            }
+        });
 
-				// 		}
+        console.log('Validated 1' + validated);
 
 
+        // $.ajax({
+        // 	url: '/mailer/recaptcha.php',
+        // 	type: 'POST',
+        // 	data: form.serialize(),
+        // 	error: function() {
+        // 		alert('An error occured while processing the request. Please try again later.');
+        // 	},
+        // 	complete : function(data) {
 
-						if(validated) {
-							console.log('inside validated');
+        // 		if(data.responseText == '') {
+        // 			validated = true;
+        // 			jQuery('#recaptcha-msg').html('');
+        // 		} else {
+        // 			validated = false;
+        // 			jQuery('#recaptcha-msg').html(data.responseText);
 
-							$.ajax({
-								url: '/mailer/mail.php',
-								type: 'POST',
-								data: form.serialize(),
-								error: function() {
-									alert('An error occured while processing the request. Please try again later.');
-								},
-								success: function(response) {
-									var data = jQuery.parseJSON(response);
-									//alert(data);
-									console.log(data);
-									if(data.success === true) {
-										jQuery('#success-form-msg').html(data.msg).show();
-									} else {
-										jQuery('#recaptcha-msg').html(data.msg);
-									}
-
-								}
-							});
-
-							console.log('below ajax');
-
-						}
-
-					// }
-
-				// });
+        // 		}
 
 
 
-				console.log('below validated');
+        if (validated) {
+            console.log('inside validated');
 
-				e.preventDefault();
-			});
+            $.ajax({
+                url: '/mailer/mail.php',
+                type: 'POST',
+                data: form.serialize(),
+                error: function () {
+                    alert('An error occured while processing the request. Please try again later.');
+                },
+                success: function (response) {
+                    var data = jQuery.parseJSON(response);
+                    //alert(data);
+                    console.log(data);
+                    if (data.success === true) {
+                        jQuery('#success-form-msg').html(data.msg).show();
+                    } else {
+                        jQuery('#recaptcha-msg').html(data.msg);
+                    }
+
+                }
+            });
+
+            console.log('below ajax');
+
+        }
+
+        // }
+
+        // });
 
 
 
+        console.log('below validated');
 
+        e.preventDefault();
+    });
+
+
+
+    window.onscroll = function () {
+        myFunction()
+    };
+
+    var header = document.getElementById("navHeader");
+    var sticky = header.offsetTop;
+
+    function myFunction() {
+        if (window.pageYOffset > sticky) {
+            header.classList.add("sticky");
+        } else {
+            header.classList.remove("sticky");
+        }
+    }
 
 });
